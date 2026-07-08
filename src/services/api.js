bbf70dd124c8e57c76a63046991fb48817aa9bca
Ashwani93333 <ashwanikumar93333@gmail.com>
2026-07-09 00:18:19 +0530

    seperate local dev and production dev .

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_API_URL_PROD;
 
 const getCsrfToken = () => {
   const match = document.cookie.match(new RegExp('(^| )XSRF-TOKEN=([^;]+)'));
